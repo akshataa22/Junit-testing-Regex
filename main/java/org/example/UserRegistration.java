bdd371cfd575833543ba5442f7f60866a4782840
akshataa22 <dhanawadeakshata53@gmail.com>
2023-12-18 22:06:55 +0530

    valid last name testing

--- main/java/org/example/UserRegistration.java
+++ main/java/org/example/UserRegistration.java
@@ -1,13 +1,20 @@
 package org.example;
 
-import java.util.Scanner;
 import java.util.regex.Pattern;
+
 
 public class UserRegistration {
     public static boolean firstName(String name){
         boolean isValidFirstName = Pattern.matches("^[A-Z]{1}[a-z]{0,20}",name);
         if(isValidFirstName == false) {
             System.out.println("Please enter valid name first name (starting with Capital letter and has minimum 3 letters).");
+        }
+        return true;
+    }
+    public static boolean lastName(String name){
+    boolean isValidLastName = Pattern.matches("^[A-Z]{1}[a-z]{0,20}",name);
+        if(isValidLastName == false) {
+            System.out.println("Please enter valid surname (Last name should start with capital letter and has minimum 3 characters).");
         }
         return true;
     }
--- test/java/com/testCases/TestCasesForUserRegistration.java
+++ test/java/com/testCases/TestCasesForUserRegistration.java
@@ -5,10 +5,16 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestCasesForUserRegistration {
+    UserRegistration user = new UserRegistration();
     @Test
     public void firstNameCheck() {
-        UserRegistration user = new UserRegistration();
         boolean result = user.firstName("Akshata");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void lastNameCheck() {
+        boolean result = user.lastName("Dhanawade");
         Assert.assertEquals(true,result);
     }
 }
