60244d1d229f0aadd4e9aa44ffe6fabbcbdc4a40
akshataa22 <dhanawadeakshata53@gmail.com>
2023-12-18 23:55:44 +0530

    custom exception for invalid user details

--- main/java/org/example/UserRegistration.java
+++ main/java/org/example/UserRegistration.java
@@ -33,4 +33,9 @@ public class UserRegistration {
 
         return isValidPassword;
     }
+
+    public static boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        return Pattern.matches(emailRegex, email);
+    }
 }
--- test/java/com/testCases/TestCasesForUserRegistration.java
+++ test/java/com/testCases/TestCasesForUserRegistration.java
@@ -7,70 +7,91 @@ import org.junit.Test;
 public class TestCasesForUserRegistration {
     UserRegistration user = new UserRegistration();
 
-    //Happy Test Cases
+    // Happy Test Cases
     @Test
     public void firstNameCheck() {
         boolean result = user.firstName("Akshata");
-        Assert.assertEquals(true,result);
+        Assert.assertEquals(true, result);
     }
 
     @Test
     public void lastNameCheck() {
         boolean result = user.lastName("Dhanawade");
-        Assert.assertEquals(true,result);
+        Assert.assertEquals(true, result);
     }
 
     @Test
-    public void EmailIdCheck()
-    {
+    public void emailIdCheck() {
         boolean result = user.emailID("dhanawadeakshata53@gmail.com");
-        Assert.assertEquals(true,result);
+        Assert.assertEquals(true, result);
     }
 
     @Test
-    public void MobileNoCheck()
-    {
+    public void mobileNoCheck() {
         boolean result = user.mobileNumber("91 1234567890");
-        Assert.assertEquals(true,result);
+        Assert.assertEquals(true, result);
     }
 
     @Test
-    public void password()
-    {
+    public void password() {
         boolean result = user.password("Admin@123");
-        Assert.assertEquals(true,result);
+        Assert.assertEquals(true, result);
     }
 
-    //Sad Test cases
-    @Test
-    public void testInvalidFirstName()
-    {
-        boolean result = user.firstName("akshata");
-        Assert.assertEquals(true,result);
-    }
-    @Test
-    public void testInvalidLastName()
-    {
-        boolean result = user.lastName("d");
-        Assert.assertEquals(true,result);
-    }
-    @Test
-    public void testInvalidEmailId()
-    {
-        boolean result = user.emailID("dgmail.com");
-        Assert.assertEquals(true,result);
-    }
-    @Test
-    public void testInvalidMobileNumber()
-    {
-        boolean result = user.mobileNumber("91 98765");
-        Assert.assertEquals(true,result);
+    // Sad Test cases
+    @Test(expected = InvalidFirstNameException.class)
+    public void testInvalidFirstName() {
+        user.firstName("akshata");
     }
 
-    @Test
-    public void testInvalidPassword()
-    {
-        boolean result = user.password("abc@123");
-        Assert.assertEquals(true,result);
+    @Test(expected = InvalidLastNameException.class)
+    public void testInvalidLastName() {
+        user.lastName("d");
+    }
+
+    @Test(expected = InvalidEmailIdException.class)
+    public void testInvalidEmailId() {
+        user.emailID("dgmail.com");
+    }
+
+    @Test(expected = InvalidMobileNumberException.class)
+    public void testInvalidMobileNumber() {
+        user.mobileNumber("91 98765");
+    }
+
+    @Test(expected = InvalidPasswordException.class)
+    public void testInvalidPassword() {
+        user.password("abc@123");
+    }
+
+    // Custom Exception classes
+    public static class InvalidFirstNameException extends Exception {
+        public InvalidFirstNameException(String message) {
+            super(message);
+        }
+    }
+
+    public static class InvalidLastNameException extends Exception {
+        public InvalidLastNameException(String message) {
+            super(message);
+        }
+    }
+
+    public static class InvalidEmailIdException extends Exception {
+        public InvalidEmailIdException(String message) {
+            super(message);
+        }
+    }
+
+    public static class InvalidMobileNumberException extends Exception {
+        public InvalidMobileNumberException(String message) {
+            super(message);
+        }
+    }
+
+    public static class InvalidPasswordException extends Exception {
+        public InvalidPasswordException(String message) {
+            super(message);
+        }
     }
 }
